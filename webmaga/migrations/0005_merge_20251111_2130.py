53305bb6842ef240e3856d6264080d435be33b2f
Juan Díaz <jchendiaz15@gmail.com>
2025-11-11 12:45:24 -0600

    merge

--- webmaga/migrations/0005_merge_20251111_2130.py
+++ webmaga/migrations/0005_merge_20251111_2130.py
@@ -5,7 +5,7 @@ class Migration(migrations.Migration):
 
     # Unifica las ramas 0003_seed_initial_data y 0004_add_grupo_id_to_evento_cambio_colaborador
     dependencies = [
-        ('webmaga', '0003_seed_initial_data'),
+        ('webmaga', '0003_initial'),
         ('webmaga', '0004_add_grupo_id_to_evento_cambio_colaborador'),
     ]
 
